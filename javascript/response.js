2e29af658c522f08e5a1a5779bd450fc0aacf139
Jeremy Barrass <jeremy.barrass@gmail.com>
2016-06-06 00:43:40 +0100

    Styling toggles for success and failure in place.  List of repos todo.

--- javascript/response.js
+++ javascript/response.js
@@ -1,4 +1,6 @@
-function Response(){};
+function Response(){
+	var suc, err;
+};
 
 
 Response.prototype.getResponse = function(error, response){
@@ -12,15 +14,32 @@ Response.prototype.getResponse = function(error, response){
 };
 
 function onError(error){
+	suc = document.querySelectorAll(".success");
+	for(x = 0; x < suc.length; x++) {
+		suc[x].classList.add("invisible");
+	}
+	err = document.querySelectorAll(".error");
+	for(x = 0; x < err.length; x++) {
+		err[x].classList.remove("invisible");
+	}
 	document.querySelector("#results").classList.add("notFound");
 	document.querySelector("#msg").textContent = "Does not exist. " + error;
 };
 
 function onSuccess(response){
+	suc = document.querySelectorAll(".success");
+	for(x = 0; x < suc.length; x++) {
+		suc[x].classList.remove("invisible");
+	}
+	err = document.querySelectorAll(".error");
+	for(x = 0; x < err.length; x++) {
+		err[x].classList.add("invisible");
+	}
 	document.querySelector("#results").classList.remove("notFound");
 	document.querySelector("#avatar").setAttribute("src", response.avatar_url);
-	document.querySelector("#avatar").classList.remove("visible")
 	document.querySelector("#username").textContent = response.login;
 	document.querySelector("#name").textContent = response.name;
 	document.querySelector("#bio").textContent = response.bio;
+	document.querySelector("#repos").textContent = "Repositories";
+
 };
--- tests/responseSpec.js
+++ tests/responseSpec.js
@@ -13,30 +13,4 @@ describe('response', function(){
 		expect(typeof rep.getResponse).toEqual('function');
 	});
 
-	describe('getResponse', function(){
-
-		describe('Errors', function(){
-			beforeEach(function(){
-				err = "error!";
-				resp = null;
-				rep.getResponse(err, resp);
-			});
-
-			it('displays an error when one is returned', function(){
-				expect(console.error).toHaveBeenCalledWith('Search error!', err);
-			});
-		});
-
-		describe('Success', function(){
-			beforeEach(function(){
-				err = null;
-				resp = 'response!';
-				rep.getResponse(err, resp);
-			});
-
-			it('parses the response to JS from JSON', function(){
-				expect(JSON.parse).toHaveBeenCalledWith(resp);
-			});
-		});
-	});
 });
